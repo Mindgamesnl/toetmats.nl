2dfb7d48c0a7e4604657718049a2870a05505de2
Mats <mats@craftmend.com>
2021-12-21 02:34:48 +0100

    Update loader.js

--- assets/loader.js
+++ assets/loader.js
@@ -46,6 +46,23 @@ async function load() {
                     </p>`
     }
 
+    for (let i = 0; i < data.Vods.length; i++) {
+        let vod = data.Vods[i]
+        document.getElementById("vods").innerHTML += `
+        <div class="p-4 md:w-1/3 sm:mb-0 mb-6 vod" onclick="window.location.href = '` + vod.Link + `'">
+                <div class="rounded-lg h-64 overflow-hidden">
+                    <img alt="content" class="object-cover object-center h-full w-full" src="` + vod.Thumbnail + `">
+                </div>
+                <h2 class="text-xl font-medium title-font text-white mt-5">` + vod.Title + `</h2>
+                <p class="text-base leading-relaxed mt-2">` + vod.Description + `</p>
+                <a class="text-pink-400 inline-flex items-center mt-3">Bekijk de stream
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                </a>
+            </div>`
+    }
+
 }
 
 load()

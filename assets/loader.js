81b753b69b502af215c829cb98fee443c5242b9d
Mats <mats@craftmend.com>
2021-12-23 18:56:33 +0100

    Update loader.js

--- assets/loader.js
+++ assets/loader.js
@@ -1,6 +1,7 @@
 async function load() {
     let response = await fetch("https://toetmats.nl/api/leaderboard")
     let data = await response.json()
+    console.log(data)
 
     function findUser(id) {
         return data.Users[id]
@@ -79,24 +80,24 @@ function secondsToString (milliseconds) {
     var temp = Math.floor(milliseconds / 1000);
     var years = Math.floor(temp / 31536000);
     if (years) {
-        return years + ' year' + numberEnding(years);
+        return years + ' jaar' + numberEnding(years);
     }
     //TODO: Months! Maybe weeks?
     var days = Math.floor((temp %= 31536000) / 86400);
-    if (days) {
-        return days + ' day' + numberEnding(days);
-    }
     var hours = Math.floor((temp %= 86400) / 3600);
+    if (days) {
+        return days + ' dag' + numberEnding(days) + " " + hours + " uur";
+    }
     if (hours) {
-        return hours + ' hour' + numberEnding(hours);
+        return hours + ' uur' + numberEnding(hours);
     }
     var minutes = Math.floor((temp %= 3600) / 60);
     if (minutes) {
-        return minutes + ' minute' + numberEnding(minutes);
+        return minutes + ' min' + numberEnding(minutes);
     }
     var seconds = temp % 60;
     if (seconds) {
-        return seconds + ' second' + numberEnding(seconds);
+        return seconds + ' sec' + numberEnding(seconds);
     }
     return 'less than a second'; //'just now' //or other string you like;
 }
